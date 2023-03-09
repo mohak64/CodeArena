8977584f5cf93f6739a899f535ef8d1583eb19b3
Jaihanuman64 <mohak64bansal@gmail.com>
2023-03-09 23:04:24 +0530

    reset password updated

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,7 +8,7 @@ export default async function Auth(req, res, next){
         // access authorize header to validate request
         const token = req.headers.authorization.split(" ")[1];
 
-        // retrive the user details fo the logged in user
+        // retrive the user details of the logged in user
         const decodedToken = await jwt.verify(token, ENV.JWT_SECRET);
 
         req.user = decodedToken;
