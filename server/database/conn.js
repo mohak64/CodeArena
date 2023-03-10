4ab90a2e2c22c729db77f922df2d8a7b7ecf3b70
Jaihanuman64 <mohak64bansal@gmail.com>
2023-03-10 18:59:02 +0530

    Login complete

--- server/database/conn.js
+++ server/database/conn.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 import { MongoMemoryServer } from "mongodb-memory-server";
-// import ENV from '../config.js'
+import ENV from '../config.js'
 
 async function connect(){
 
@@ -9,8 +9,8 @@ async function connect(){
     const getUri = mongod.getUri();
 
     mongoose.set('strictQuery', true)
-    const db = await mongoose.connect(getUri);
-    // const db = await mongoose.connect(ENV.ATLAS_URI);
+    //const db = await mongoose.connect(getUri);
+     const db = await mongoose.connect(ENV.ATLAS_URI);
     console.log("Database Connected")
     return db;
 }
